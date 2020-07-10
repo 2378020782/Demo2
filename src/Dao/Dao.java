81ab4fe5535ffe7d32280320d987f5800dfaee86
yy <2378020782@qq.com>
2020-07-10 20:40:58 +0800

    判断传递的数据是否为空

--- src/Dao/Dao.java
+++ src/Dao/Dao.java
@@ -1,37 +1,29 @@
 package Dao;
 
 import java.sql.Connection;
-  import java.sql.Statement;
+import java.sql.Statement;
+import DBUtil.DBUtil;
+import Entity.User;
 
-          import DBUtil.DBUtil;
-
-          import Entity.User;
-
-          public class Dao {
-
-              public boolean add(User user) {
+     public class Dao {
+         public boolean add(User user) {
                  String sql = "insert into uses1 (username,password,sex,name,stuname,email,xueyuan,xi,banji,year,address,addtext) values('"+ user.getUsername() + "','"+ user.getPassword() +"','"+ user.getSex() +"','"+user.getName() +"','"+ user.getStuname() +"','"+user.getEmail()+"','"+ user.getXueyuan() +"','"+user.getXi()+"','"+user.getBanji()+"','"+user.getYear()+"','"+user.getAddress()+"','"+user.getAddtext()+"')";
                  Connection conn = DBUtil.getConn();
                  Statement state = null;
                  boolean f = false;
                  int a = 0;
-
                  try {
                          state = conn.createStatement();
                          a=state.executeUpdate(sql);
                      } catch (Exception e) {
                          e.printStackTrace();
                      } finally {
-                         // 关闭z 连接
                          DBUtil.close(state, conn);
                      }
-
                  if (a > 0) {
                          f = true;
                      }
                  return f;
 
          }
-
-
-         }
+     }
