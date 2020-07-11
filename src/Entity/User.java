0c2a2d877209441cc4112dca45d87b8eca1aa4b9
yy <2378020782@qq.com>
2020-07-11 10:25:26 +0800

    传入数据

--- src/Entity/User.java
+++ src/Entity/User.java
@@ -15,102 +15,102 @@ public class User {
     private String address;
     private String addtext;
 
-              public String getUsername() {
+    public String getUsername() {
                  return username;
              }
 
-              public void setUsername(String username) {
+    public void setUsername(String username) {
                  this.username = username;
              }
 
-              public String getPassword() {
+    public String getPassword() {
                  return password;
              }
 
-              public void setPassword(String password) {
+    public void setPassword(String password) {
                  this.password = password;
              }
 
-              public String getSex() {
+    public String getSex() {
                  return sex;
              }
 
-              public void setSex(String sex) {
+    public void setSex(String sex) {
                  this.sex = sex;
              }
 
-              public String getName() {
+    public String getName() {
                  return name;
              }
 
-              public void setName(String name) {
+    public void setName(String name) {
                  this.name = name;
              }
 
-              public String getStuname() {
+    public String getStuname() {
                  return stuname;
              }
 
-              public void setStuname(String stuname) {
+    public void setStuname(String stuname) {
                  this.stuname = stuname;
              }
 
-              public String getEmail() {
+    public String getEmail() {
                  return email;
              }
 
-              public void setEmail(String email) {
+    public void setEmail(String email) {
                  this.email = email;
              }
 
-              public String getXueyuan() {
+    public String getXueyuan() {
                  return xueyuan;
              }
 
-              public void setXueyuan(String xueyuan) {
+    public void setXueyuan(String xueyuan) {
                  this.xueyuan = xueyuan;
              }
 
-              public String getXi() {
+    public String getXi() {
                  return xi;
              }
 
-              public void setXi(String xi) {
+    public void setXi(String xi) {
                  this.xi = xi;
              }
 
-              public String getBanji() {
+    public String getBanji() {
                  return banji;
              }
 
-              public void setBanji(String banji) {
+    public void setBanji(String banji) {
                  this.banji = banji;
              }
 
-              public String getYear() {
+    public String getYear() {
                  return year;
              }
 
-              public void setYear(String year) {
+    public void setYear(String year) {
          this.year = year;
     }
-              public String getAddress() {
+    public String getAddress() {
                  return address;
              }
 
-             public void setAddress(String address) {
+    public void setAddress(String address) {
                  this.address = address;
              }
 
-             public String getAddtext() {
+    public String getAddtext() {
                  return addtext;
              }
 
-             public void setAddtext(String addtext) {
+    public void setAddtext(String addtext) {
                  this.addtext = addtext;
              }
 
-             public User() {}
+    public User() {}
 
          public User(String username,String password,String sex,String name,String stuname,String email,String xueyuan, String xi,String banji,String year,String address,String addtext) {
 
